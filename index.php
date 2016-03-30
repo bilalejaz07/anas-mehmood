aaa0d1c9fc7902d65bdf6390e9abb14bc7cb0ae4
bilalejaz07 <bilalejaz07@hotmail.com>
2016-03-31 02:50:38 +0500

    add me

---
+++ index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Heroku App</title>
+<link rel="icon" href ="images/5.ico" type="image/x-icon">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap-3.3.4-dist/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+<style>
+.navbar{
+margin-top:-5%;}
+body{
+
+}
+
+
+th{
+font-size:90%;
+font-family:calibri;}
+
+</style>
+</head>
+
+
+<body>
+<div class="container">
+
+
+
+ <div class="jumbotron">
+<h1 align='center'>This is my testing app on Heroku</h1>
+
+
+</div>
+
+</div>
+
+
+
+
+
+
+</body>
+
+</html>
